e0b696da203556e5a2ae3dc4577cc10a9c55a9e2
Gustaf Broström <gustafbrostrom@Gustafs-MacBook-Air.local>
2022-10-06 21:06:36 +0200

    m

--- main.py
+++ main.py
@@ -45,28 +45,31 @@ if __name__ == '__main__':
         mouse_pos = pygame.mouse.get_pos()
         gw.displayInitialBoard()
 
-        q,r = _pixel_to_hex(*mouse_pos)
-        land = board.lands[(q,r)]
-        tile = land['hextile']
-        last_curr_closest = (float('inf'), None, None)
-        curr_closest = (float('inf'), None, None)
-        for c in tile.corners:
-            d = dist(mouse_pos, c)
-            if d < curr_closest[0]:
-                last_curr_closest = (curr_closest[0], curr_closest[1], curr_closest[2])
-                curr_closest = (d, c, land)
+        try:
+            q,r = _pixel_to_hex(*mouse_pos)
+            land = board.lands[(q,r)]
+            tile = land['hextile']
+            last_curr_closest = (float('inf'), None, None)
+            curr_closest = (float('inf'), None, None)
+            for c in tile.corners:
+                d = dist(mouse_pos, c)
+                if d < curr_closest[0]:
+                    last_curr_closest = (curr_closest[0], curr_closest[1], curr_closest[2])
+                    curr_closest = (d, c, land)
 
-        v1 = sub(last_curr_closest[1], mouse_pos)
-        v2 = sub(curr_closest[1], mouse_pos)
-        dot_prod = dot(v1,v2)/(norm(v1)* norm(v2))
-        if dot_prod < 0.5:
-            pygame.draw.rect(gw.screen, pygame.Color('green'), (curr_closest[1][0]-8,curr_closest[1][1]-8,16, 16))
-        else:
-            pygame.draw.line(gw.screen, pygame.Color('green'), curr_closest[1], last_curr_closest[1], 3)
+            v1 = sub(last_curr_closest[1], mouse_pos)
+            v2 = sub(curr_closest[1], mouse_pos)
+            dot_prod = dot(v1,v2)/(norm(v1)* norm(v2))
+            if dot_prod < 0.5:
+                pygame.draw.rect(gw.screen, pygame.Color('green'), (curr_closest[1][0]-8,curr_closest[1][1]-8,16, 16))
+            else:
+                pygame.draw.line(gw.screen, pygame.Color('green'), curr_closest[1], last_curr_closest[1], 3)
 
-        if DEBUG:
-            pygame.draw.line(gw.screen, pygame.Color('green'), mouse_pos, last_curr_closest[1], 1)
-            pygame.draw.line(gw.screen, pygame.Color('green'), curr_closest[1], mouse_pos, 1)
+            if DEBUG:
+                pygame.draw.line(gw.screen, pygame.Color('green'), mouse_pos, last_curr_closest[1], 1)
+                pygame.draw.line(gw.screen, pygame.Color('green'), curr_closest[1], mouse_pos, 1)
+        except Exception:
+            pass
 
 
         for event in ev:
